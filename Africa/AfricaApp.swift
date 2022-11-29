9649e0662e3f5e537d5bc0c938cdcf9cc68eeb2e
Flo2503 <bruel.florent@gmail.com>
2022-11-29 19:39:05 +0100

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Florent on 29/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
