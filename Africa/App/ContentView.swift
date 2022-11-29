fa0d2379ff55461b46d98d4abdca500240ba3026
Flo2503 <bruel.florent@gmail.com>
2022-11-29 20:13:22 +0100

    Add assets, images, videos, colors. Created launch screen in info panel. Added folders to the project with principale views. Added tab view

---
+++ Africa/App/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Florent on 29/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Content")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Africa/App/GaleryView.swift
@@ -0,0 +1,20 @@
+//
+//  GaleryView.swift
+//  Africa
+//
+//  Created by Florent on 29/11/2022.
+//
+
+import SwiftUI
+
+struct GaleryView: View {
+    var body: some View {
+        Text("Galery")
+    }
+}
+
+struct GaleryView_Previews: PreviewProvider {
+    static var previews: some View {
+        GaleryView()
+    }
+}
