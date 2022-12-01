ce154b1075c2cb43876af1c1347fcbc45becd9ed
Flo2503 <bruel.florent@gmail.com>
2022-12-01 19:23:24 +0100

    Created extension Bundle to decode Json cover images

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Florent on 29/11/2022.
+//
+
+import SwiftUI
+
+struct CoverImage: Decodable, Identifiable {
+    let id: Int
+    let name: String
+}
