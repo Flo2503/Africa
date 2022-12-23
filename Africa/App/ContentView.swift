7505a429126c57b44135ad462a4021bedd567c0b
Flo2503 <bruel.florent@gmail.com>
2022-12-23 17:40:48 +0100

    Created animal detail with headline, gallery and title

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -20,7 +20,9 @@ struct ContentView: View {
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
                 ForEach(animals) { animal in
-                    AnimaListItemView(animal: animal)
+                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                        AnimaListItemView(animal: animal)
+                    } //: LINK
                 }
             } //: LIST
             .navigationBarTitle("Africa", displayMode: .large)
